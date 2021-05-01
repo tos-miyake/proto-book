a249c04de22df3eba309458a8a7c4ddb40505d40
tos-miyake <tosihiro0917@gmail.com>
2021-05-01 15:58:47 +0900

    見た目を整える

--- sample-app/src/pages/[userId]/[articleId]/index.tsx
+++ sample-app/src/pages/[userId]/[articleId]/index.tsx
@@ -1,7 +1,12 @@
 import { useRouter } from 'next/router'
 import { NextPage } from 'next'
+import Error from 'next/error'
 
 import { useGetArticleQuery } from '@/generated/graphql'
+
+import styles from './index.module.css'
+
+import { Article } from '@/components/article'
 
 const ArticlePage: NextPage = () => {
     const router = useRouter()
@@ -19,7 +24,30 @@ const ArticlePage: NextPage = () => {
     if (error) {
         return <p>{error.toString()}</p>
     }
-    return <div>{JSON.stringify(data)}</div>
+
+    if (!data || !data.articles_by_pk) {
+        return <Error statusCode={404} />
+    }
+
+    const { user, subject, content } = data.articles_by_pk
+
+    return (
+        <div className={styles.contentContainer}>
+            <h1 className={styles.subject}>{subject}</h1>
+            <div className={styles.userContainer}>
+                <div>
+                    <img className={styles.userIcon} src="/profile.png" />
+                </div>
+                <div className={styles.userText}>
+                    <div className={styles.userId}>{user.displayId}</div>
+                    <span className={styles.userName}>{user.displayName}</span>
+                </div>
+            </div>
+            <div className={styles.content}>
+                <Article content={content} />
+            </div>
+        </div>
+    )
 }
 
 export default ArticlePage
