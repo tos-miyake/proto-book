7dc3fc5f115f7aa32d57bdfa8cc60944b1b361c4
tos-miyake <tosihiro0917@gmail.com>
2021-05-04 16:31:45 +0900

    site header 投稿画面に追加

--- sample-app/src/pages/post/index.tsx
+++ sample-app/src/pages/post/index.tsx
@@ -4,6 +4,7 @@ import { useState, useCallback } from 'react'
 import { Editor } from '@/components/editor'
 
 import styles from './index.module.css'
+import { SiteHeader } from '@/components/site-header'
 
 const PostPage: NextPage = () => {
     const [subject, setSubject] = useState('')
@@ -17,21 +18,24 @@ const PostPage: NextPage = () => {
     )
 
     return (
-        <div className={styles.editContent}>
-            <input
-                className={styles.subject}
-                type="text"
-                placeholder="タイトル"
-                value={subject}
-                onChange={handleChangeSubject}
-            />
-            <Editor
-                className={styles.editor}
-                placeholder="本文を書きましょう"
-                value={content}
-                onEdit={setContent}
-            />
-        </div>
+        <>
+            <SiteHeader />
+            <div className={styles.editContent}>
+                <input
+                    className={styles.subject}
+                    type="text"
+                    placeholder="タイトル"
+                    value={subject}
+                    onChange={handleChangeSubject}
+                />
+                <Editor
+                    className={styles.editor}
+                    placeholder="本文を書きましょう"
+                    value={content}
+                    onEdit={setContent}
+                />
+            </div>
+        </>
     )
 }
 
