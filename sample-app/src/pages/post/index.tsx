4762307508b01925a3ce14af95f96ba7c627acc7
tos-miyake <tosihiro0917@gmail.com>
2021-05-04 17:27:13 +0900

    投稿する

--- sample-app/src/pages/post/index.tsx
+++ sample-app/src/pages/post/index.tsx
@@ -1,15 +1,20 @@
 import { NextPage } from 'next'
 import { useState, useCallback } from 'react'
+import { useRouter } from 'next/router'
 
 import { Editor } from '@/components/editor'
 
 import styles from './index.module.css'
 import { SiteHeader, SiteHeaderItem } from '@/components/site-header'
 import { Button } from '@/components/button'
+import { usePostArticleMutation } from '@/generated/graphql'
 
 const PostPage: NextPage = () => {
     const [subject, setSubject] = useState('')
     const [content, setContent] = useState('')
+    const [postArticle] = usePostArticleMutation()
+    const [postDisabled, setPostDisabled] = useState(false)
+    const router = useRouter()
 
     const handleChangeSubject = useCallback(
         (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,12 +22,45 @@ const PostPage: NextPage = () => {
         },
         [],
     )
+
+    const handlePost = useCallback(
+        async (ev: React.FormEvent<HTMLFormElement>) => {
+            ev.preventDefault()
+            if (!content || !subject || postDisabled) {
+                return
+            }
+
+            setPostDisabled(true)
+            const { data } = await postArticle({
+                variables: {
+                    // FIXME: authorIdは一旦決め打ちにする
+                    authorId: '46d0d949-2d48-43b1-ba3e-a8732d16cd3e',
+                    content,
+                    subject,
+                    publishedAt: 'now()',
+                },
+            })
+            if (data && data.insert_articles_one) {
+                const articleId = data.insert_articles_one.id
+                // FIXME ユーザーID決め打ち
+                router.push(`/hoge/${articleId}`)
+                setPostDisabled(false)
+            } else {
+                console.log('POST unknown state', data)
+            }
+        },
+        [content, subject, postDisabled, postArticle, router],
+    )
+
+
     const siteHeaderRight = (
         <>
             <SiteHeaderItem>
-                <Button type="submit">
-                    <span>投稿する</span>
-                </Button>
+                <form onSubmit={handlePost}>
+                    <Button type="submit">
+                        <span>投稿する</span>
+                    </Button>
+                </form>
             </SiteHeaderItem>
             <SiteHeaderItem>
                 <img className={styles.userIcon} src="/profile.png" />
